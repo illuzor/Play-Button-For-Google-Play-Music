7cfa862dd4fb31b0007a75c89a6f3300cc77515c
illuzor <illuzor@gmail.com>
2017-02-15 17:40:45 +0300

    first commit

---
+++ source/action-play.js
@@ -0,0 +1,15 @@
+﻿var result = "none";
+
+if (document.querySelectorAll('[data-id="play-pause"]').length) {
+    var button = document.querySelectorAll('[data-id="play-pause"]')[0];
+
+    if (button.getAttribute("aria-disabled") != "true") {
+        button.click()
+        if (button.getAttribute("class").includes(" playing"))
+            result = "pause";
+        else
+            result = "play";
+    }
+}
+
+result;
---
+++ source/background.js
@@ -0,0 +1,33 @@
+﻿chrome.browserAction.onClicked.addListener(function() {
+	chrome.windows.getAll( { populate: true }, getWindows);
+});
+
+function getWindows(windows) {
+    var gmTabs = [];
+    for (var i = 0; i < windows.length; i++) {
+        for (var j = 0; j < windows[i].tabs.length; j++) {
+            if (windows[i].tabs[j].url.includes("//play.google.com/music"))
+                gmTabs.push(windows[i].tabs[j]);
+        }
+    }
+
+    if (gmTabs.length) {
+		chrome.tabs.executeScript(gmTabs[0].id, { file: "action-play.js" }, playPause);
+	} else {
+		chrome.tabs.create({ url: "https://play.google.com/music/listen" });
+	}  
+}
+
+function playPause(result) {
+    var iconPath;
+    var iconText;
+    if (result == "play") {
+        iconPath = "images/pause.png";
+        iconText = "Pause";
+    } else {
+        iconPath = "images/play.png";
+        iconText = "Play";
+    }
+    chrome.browserAction.setIcon({ path: iconPath });
+    chrome.browserAction.setTitle({ title: iconText });
+}
