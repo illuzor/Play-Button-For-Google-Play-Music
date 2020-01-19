504fa6bc00642ada7f8b230226fac1897fd2aeca
illuzor <illuzor@gmail.com>
2020-01-19 19:17:50 +0300

    Button state detection fix

--- source/action-play.js
+++ source/action-play.js
@@ -3,7 +3,7 @@ var result = "Play";
 var button = document.querySelector('[data-id="play-pause"]')
 if (button.getAttribute("aria-disabled") != "true") {
     button.click()
-    if (!button.getAttribute("class").includes(" playing"))
+    if (!button.getAttribute("class").includes("playing"))
         result = "Pause";
 }
 result;
